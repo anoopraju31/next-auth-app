ffd8515a4c26452b6d361b95768db8b0a58e8bb3
Anoop Raju <anoopraju31@gmail.com>
2023-08-04 11:56:47 +0530

    Sign In page : styled

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -25,7 +25,7 @@ export default function SignUpPage() {
 
 				<div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
 					<div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
-						<h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+						<h1 className='text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
 							Create your account
 						</h1>
 
