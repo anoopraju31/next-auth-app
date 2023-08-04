6adc5bca16f9bfd43a47051fbcfee11ffd8b2a78
Anoop Raju <anoopraju31@gmail.com>
2023-08-04 10:06:03 +0530

    Sign Up Page - initial

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+	return (
+		<section className='h-screen flex justify-center items-center'>
+			<h1 className='text-center text-2xl text-white'> Sign Up Page </h1>
+		</section>
+	)
+}
